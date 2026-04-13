d5a700d37905f0997b52bdcd61fbcfab7453e7c2
Amrendra Singh <happysingh0987688@gmail.com>
2026-04-14 01:18:19 +0530

    Add distinct_numbers solution
    Add new Sorting and Searching/distinct_numbers.cpp. Implements a solution template with fast IO, common utility macros and functions (gcd, lcm, modpow, modinv), and a solve() that reads n integers into a set and prints the number of distinct values. Contains a TODO comment for further problem-specific logic and runs a single test by default.

---
+++ Sorting and Searching/distinct_numbers.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a); i<(b); i++)
+#define rloop(i,a,b) for(int i=(a); i>=(b); i--)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e8;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll modpow(ll a, ll b, ll mod=MOD) {
+    ll res = 1;
+    while(b){
+        if(b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+ll modinv(ll a, ll mod = MOD) {
+return modpow(a, mod - 2, mod);
+}
+
+void solve(){
+    int n; cin >> n;
+    set<int> a;
+
+    loop(i,0,n) {
+        int x;cin >> x;
+    a.insert(x);
+}
+    // TODO: Add problem logic
+    cout << a.size();
+}
+
+int main(){
+    fast_io;
+    int t=1;
+   // cin >> t;
+    while(t--) solve();
+    return 0;
+}
