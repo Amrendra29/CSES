c7051b5b7e9b2f15c6703ec6dc7af0dcca393697
Amrendra Singh <happysingh0987688@gmail.com>
2026-04-16 00:13:22 +0530

    Solved CSES Concert tickets

---
+++ Sorting and Searching/concert_tickets.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define ll long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define loop(i,a,b) for(int i=(a); i<(b); i++)
+#define rloop(i,a,b) for(int i=(a); i>=(b); i--)
+
+const int MOD = 1e9 + 7;
+const int INF = 1e8;
+
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+ll modpow(ll a, ll b, ll mod=MOD) {
+    ll res = 1;
+    while(b){
+        if(b & 1) res = res * a % mod;
+        a = a * a % mod;
+        b >>= 1;
+    }
+    return res;
+}
+ll modinv(ll a, ll mod = MOD) {
+return modpow(a, mod - 2, mod);
+}
+
+void solve(){
+    int n, m; cin >> n >> m;
+    vector<int> b(m);
+    multiset<int>a;
+    loop(i,0,n) {int x;cin >> x; a.insert(x);}
+    loop(i,0,m) cin>>b[i];
+   for(int i = 0; i < m; i++){
+        int cur = b[i];
+
+        auto it = a.upper_bound(cur);
+
+        if(it == a.begin()){
+            cout << -1 << " ";
+        } else {
+            --it;
+            cout << *it << " ";
+            a.erase(it);
+        }
+    }
+
+}
+
+int main(){
+    fast_io;
+    int t=1;
+    //cin >> t;
+    while(t--) solve();
+    return 0;
+}
